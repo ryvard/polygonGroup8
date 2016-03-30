f865b2345e3bfc359691ce1ca15b6f24db40004b
miaryvard <miaryvard@10.20.154.111>
2016-03-30 14:00:36 +0200

    Oprettelse af interfaces til controller og facade

---
+++ src/java/Logic/IController.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logic;
+
+import Domain.Building;
+
+/**
+ *
+ * @author miaryvard
+ */
+public interface IController
+{
+    /**
+     * Called when you submit your new building.
+     */
+    public void createBuilding(Building building);
+}
