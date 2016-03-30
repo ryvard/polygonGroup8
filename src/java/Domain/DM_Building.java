be6b334013ee9f85ed2d35229bbd24bcdb15b241
miaryvard <miaryvard@10.20.154.111>
2016-03-30 12:37:07 +0200

    domain commit

---
+++ src/java/Domain/DM_Building.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Domain;
+
+/**
+ *
+ * @author miaryvard
+ */
+public class DM_Building
+{
+    DatabaseConnector con = new DatabaseConnector();
+    
+    public void createBuilding()
+    {
+    }
+    
+    
+}
