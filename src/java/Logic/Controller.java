8d796590796059775e20c96dc3846fcfd3836942
miaryvard <miaryvard@10.20.154.111>
2016-03-30 13:14:18 +0200

    domain commit

--- src/java/Logic/Controller.java
+++ src/java/Logic/Controller.java
@@ -3,16 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Domain;
+package Logic;
 
 /**
  *
  * @author miaryvard
  */
-public class NewClass
+public class Controller
 {
-    // Hej HEJ HEJ
-    // farvel
-    // jeg
-    // Hej piger
+    
 }
