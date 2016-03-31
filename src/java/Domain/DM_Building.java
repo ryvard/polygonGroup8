08158596a1af226ab48f32567cd4f12b178b3550
miaryvard <miaryvard@10.20.154.111>
2016-03-31 13:35:39 +0200

    interface

--- src/java/Domain/DM_Building.java
+++ src/java/Domain/DM_Building.java
@@ -25,4 +25,6 @@ public class DM_Building
 
     }
     
+    
+    
 }
--- src/java/Domain/Facade.java
+++ src/java/Domain/Facade.java
@@ -5,22 +5,32 @@
  */
 package Domain;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author miaryvard
  */
-public class Facade
+public class Facade implements IFacade
 {
     DM_Building dm_building = new DM_Building();
     DM_Customer dm_customer = new DM_Customer();
     
+    @Override
     public void createBuilding(Building building)
     {
         dm_building.createBuildingInDB(building);
     }
     
+    @Override
     public void createCustomer(Customer customer) 
     {
         dm_customer.createCustomer(customer);
+    }
+
+    @Override
+    public ArrayList<Building> getBuildingList()
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
