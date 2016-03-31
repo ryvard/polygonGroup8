f71ce2067e64afc8636432464be05ba8493f4fd8
emmablomsterberg <emmablomsterberg@10.20.153.43>
2016-03-31 10:23:23 +0200

    opretter konstruktøren til customer objektet

--- src/java/Domain/Customer.java
+++ src/java/Domain/Customer.java
@@ -11,4 +11,80 @@ package Domain;
  */
 public class Customer {
     
+    String type;
+    String streetName;
+    String streetNo;
+    int zipcode;
+    String contactName;
+    String phone;
+    String mail;
+  
+    public Customer(String type, String streetName, String streetNo, int zipcode, String contactName, String phone, String mail) {
+        
+        this.type = type;
+        this.streetName = streetName;
+        this.streetNo = streetNo;
+        this.zipcode = zipcode;
+        this.contactName = contactName;
+        this.phone = phone;
+        this.mail = mail;
+        
+    }
+    
+    public String getType() {
+        return type;
+    }
+    
+    public void setType(String type) {
+        this.type = type;
+    }
+    
+    public String getStreetName() {
+        return streetName;
+    }
+    
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+    
+    public String getStreetNo() {
+        return streetNo;
+    }
+    
+    public void setStreetNo(String streetNo) {
+        this.streetNo = streetNo;
+    }
+    
+    public int getZipcode() {
+        return zipcode;
+    }
+    
+    public void setZipcode(int zipcode) {
+        this.zipcode = zipcode;
+    }
+    
+    public String getContactName() {
+        return contactName;
+    }
+    
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
+    }
+    
+    public String getPhone() {
+        return phone;
+    }
+    
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    
+    public String getMail() {
+        return mail;
+    }
+    
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+    
 }
