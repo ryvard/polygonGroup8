1a24f3e2c1b9c28cc14c79b2a03128e0f7a57131
miaryvard <miaryvard@10.20.153.47>
2016-04-04 13:56:37 +0200

    Opretning af report DM og objekt

---
+++ src/java/Domain/Report.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Domain;
+
+/**
+ *
+ * @author miaryvard
+ */
+public class Report
+{
+    int RepNo;
+    String buildingName;
+    String streetName;
+    String streetNumb;
+    int zipcode;
+    String city;
+    String date;
+    
+    String Picture;
+    
+    int YearOfConst;
+    double squareMeters;
+    String buildingUse;
+    
+    //damages 
+    
+    
+    int custID;
+    
+}
