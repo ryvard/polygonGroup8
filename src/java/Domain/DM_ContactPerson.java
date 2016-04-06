cff9dba57e2ce59ae6157928f18922d3a1c1712d
emmablomsterberg <emmablomsterberg@10.20.153.43>
2016-04-06 10:14:23 +0200

    hente contactperson fra database metode

--- src/java/Domain/DM_ContactPerson.java
+++ src/java/Domain/DM_ContactPerson.java
@@ -5,6 +5,10 @@
  */
 package Domain;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 /**
  *
  * @author emmablomsterberg
@@ -18,5 +22,33 @@ public class DM_ContactPerson {
         
         DatabaseConnector db_Connect = DatabaseConnector.getInstance();
         db_Connect.updateData(query);
+    }
+    
+    public ArrayList<ContactPerson> getContactPersonList() {
+        
+        ArrayList<ContactPerson> contactPerson = new ArrayList();
+        
+        String query = "SELECT * FROM ContactPerson;";
+        
+        DatabaseConnector db_Connect = DatabaseConnector.getInstance();
+        ResultSet res = db_Connect.getData(query);
+
+        try
+        {
+            while (res.next())
+            {
+                ContactPerson cp = new ContactPerson(res.getInt(1), res.getString(2), res.getString(3));
+                
+                contactPerson.add(cp);
+
+            }
+            return contactPerson;
+
+        } catch (SQLException ex)
+        {
+            System.out.println("€€€€€€€€€€€€€€€%&€#   " + ex);
+
+        }
+        return null;
     }
 }
